b88e6c15d5202d50c009e452dcc8549d66eb1e6b
jack_fan <jack.fan@airbnb.com>
2021-03-02 02:14:03 -0800

    lab 1 map reduce

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -5,14 +5,72 @@ import "net"
 import "os"
 import "net/rpc"
 import "net/http"
-
+import "fmt"
+import "sync"
+import "time"
+import "strconv"
 
 type Coordinator struct {
-	// Your definitions here.
-
+	mapJobs map[string]int64 // filename -> unix timestamp
+	reduceJobs map[string]int64 // partition index -> unix timestamp
+	lock sync.Mutex
+	partitionCount int
 }
 
 // Your code here -- RPC handlers for the worker to call.
+func (c *Coordinator) SendTask(args *SendTaskArgs, reply *SendTaskReply) error {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	for fileName, startTime := range c.mapJobs {
+		if time.Now().Unix() - startTime >= 10 {
+			reply.FileName = fileName
+			reply.TaskType = "Map"
+			reply.PartitionCount = c.partitionCount
+			c.mapJobs[fileName] = time.Now().Unix()
+			return nil
+		}
+	}
+
+	if len(c.mapJobs) > 0 { return nil }
+
+	for index, startTime := range c.reduceJobs {
+		if time.Now().Unix() - startTime >= 10 {
+			reply.TaskType = "Reduce"
+			i, err := strconv.Atoi(index)
+			if err != nil {
+				fmt.Println("welp shiet")
+				return nil
+			}
+			reply.PartitionCount = i
+			c.reduceJobs[index] = time.Now().Unix()
+			return nil
+		}
+	}
+
+	if len(c.reduceJobs) > 0 { return nil }
+
+	// nothing left to do, we send the terminate signal to kill the worker
+	reply.TaskType = "Terminate"
+
+	return nil
+}
+
+func (c *Coordinator) SignalTaskDone(args *SignalTaskDoneArgs, reply *SignalTaskDoneReply) error {
+	if args.TaskType == "Map" {
+		c.lock.Lock()
+		delete(c.mapJobs, args.FileName);
+		c.lock.Unlock()
+	} else if args.TaskType == "Reduce" {
+		c.lock.Lock()
+		delete(c.reduceJobs, strconv.Itoa(args.PartitionCount))
+		c.lock.Unlock()
+	} else {
+		fmt.Println("hmm... getting a weird signal")
+		fmt.Println(args.FileName)
+		fmt.Println(args.TaskType)
+	}
+	return nil
+}
 
 //
 // an example RPC handler.
@@ -46,12 +104,9 @@ func (c *Coordinator) server() {
 // if the entire job has finished.
 //
 func (c *Coordinator) Done() bool {
-	ret := false
-
-	// Your code here.
-
-
-	return ret
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return len(c.reduceJobs) == 0
 }
 
 //
@@ -60,10 +115,17 @@ func (c *Coordinator) Done() bool {
 // nReduce is the number of reduce tasks to use.
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	c := Coordinator{partitionCount: nReduce, mapJobs: make(map[string] int64), reduceJobs: make(map[string] int64)}
 
-	// Your code here.
+	// set the unix timestamp to 0, effectively saying the job has been running
+	// forever and is ready to be picked up "again"
+	for _, filename := range os.Args[1:] {
+		c.mapJobs[filename] = 0
+	}
 
+	for i := 0; i < nReduce; i++ {
+		c.reduceJobs[strconv.Itoa(i)] = 0
+	}
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -4,6 +4,13 @@ import "fmt"
 import "log"
 import "net/rpc"
 import "hash/fnv"
+import "strings"
+import "os"
+import "bufio"
+import "io"
+import "io/ioutil"
+import "strconv"
+import "time"
 
 
 //
@@ -24,18 +31,132 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
+// Readln returns a single line (without the ending \n)
+// from the input buffered reader.
+// An error is returned iff there is an error with the
+// buffered reader.
+func readLine(r *bufio.Reader) (string, error) {
+	var (isPrefix bool = true
+			 err error = nil
+			 line, ln []byte
+			)
+	for isPrefix && err == nil {
+			line, isPrefix, err = r.ReadLine()
+			ln = append(ln, line...)
+	}
+	return string(ln),err
+}
 
 //
 // main/mrworker.go calls this function.
 //
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
+	for {
+		reply := CallSendTask()
+		if reply.TaskType == "Map" {
+			doMapWork(mapf, reply)
+			CallSignalTaskDone(reply.FileName, reply.TaskType, reply.PartitionCount)
+		} else if reply.TaskType == "Reduce" {
+			doReduceWork(reducef, reply)
+			CallSignalTaskDone(reply.FileName, reply.TaskType, reply.PartitionCount)
+		} else if reply.TaskType == "Terminate" {
+			return
+		}
+		time.Sleep(100)
+	}
+}
 
-	// Your worker implementation here.
+func doMapWork(mapf func(string, string) []KeyValue, reply SendTaskReply) {
+	file, err := os.Open(reply.FileName)
+	if err != nil {
+		log.Fatalf("cannot open %v", reply.FileName)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", reply.FileName)
+	}
 
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+	kva := mapf(reply.FileName, string(content))
 
+	// open n files where n is partitionCount sent from coordinator
+	intermediateFiles := []*os.File{}
+	for i :=0; i < reply.PartitionCount; i++ {
+		oname := "mr-intermediate-" + strconv.Itoa(i) + "-" + getFileNameOnly(reply.FileName)
+		ofile, _ := os.Create(oname)
+		intermediateFiles = append(intermediateFiles, ofile)
+	}
+
+	for _, kv := range kva {
+		// for each word, hash the word, modulo partitionCount and append in file
+		fmt.Fprintf(intermediateFiles[ihash(kv.Key) % reply.PartitionCount], "%v %v\n", kv.Key, kv.Value)
+	}
+
+	// close all the opened files
+	for i :=0; i < reply.PartitionCount; i++ {
+		intermediateFiles[i].Close()
+	}
+}
+
+func doReduceWork(reducef func(string, []string) string, reply SendTaskReply) {
+	prefix := "mr-intermediate-" + strconv.Itoa(reply.PartitionCount)
+	resultMap := make(map[string][]string)
+
+	// loop over files and find matches of mr-intermediate-{partitionIndex}
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, fileInfo := range files {
+		// example intermediate filenames: mr-intermediate-2-pg-tom_sawyer.txt
+		// here we loop through the files under the partitionCount
+		if !strings.HasPrefix(fileInfo.Name(), prefix) { continue }
+		file, err := os.Open(fileInfo.Name())
+		if err != nil {
+			log.Fatalf("cannot open %v", fileInfo.Name())
+		}
+		reader := bufio.NewReader(file)
+		for {
+			line, err := readLine(reader)
+			if err == io.EOF { break }
+
+			lineContent := strings.Split(line, " ")
+			k := lineContent[0]
+			v := lineContent[1]
+
+			if _, ok := resultMap[k]; ok {
+				resultMap[k] = append(resultMap[k], v)
+			} else {
+				resultMap[k] = []string{v}
+			}
+		}
+	}
+
+	finalFileName := "mr-out-" + strconv.Itoa(reply.PartitionCount)
+	ofile, _ := os.Create(finalFileName)
+	for k, kva := range resultMap {
+		fmt.Fprintf(ofile, "%v %v\n", k, reducef(k, kva))
+	}
+	ofile.Close()
+}
+
+func getFileNameOnly(path string) string {
+	temp := strings.Split(path, "/")
+	return temp[len(temp)-1]
+}
+
+func CallSendTask() SendTaskReply {
+	args := SendTaskArgs{}
+	reply := SendTaskReply{}
+	call("Coordinator.SendTask", &args, &reply)
+	return reply
+}
+
+func CallSignalTaskDone(fileName string, taskType string, partitionCount int) {
+	args := SignalTaskDoneArgs{FileName: fileName, TaskType: taskType, PartitionCount: partitionCount }
+	reply := SignalTaskDoneReply{}
+	call("Coordinator.SignalTaskDone", &args, &reply)
 }
 
 //
@@ -80,6 +201,6 @@ func call(rpcname string, args interface{}, reply interface{}) bool {
 		return true
 	}
 
-	fmt.Println(err)
+	fmt.Printf("something wrong with rpc call... error=%s\n", err)
 	return false
 }
